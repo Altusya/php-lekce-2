a8e3cb3cff1fff3b0feb21d96c2fd7cdd82aaabb
Altusya <goldmoon7@email.cz>
2019-02-27 00:38:24 +0100

    trojuhelnik

--- pythagoras.php
+++ pythagoras.php
@@ -3,8 +3,19 @@ $a = 3;
 $b = 5;
 $obdelnikobsah = $a * $b;
 
-echo 'Obsah obdelníku o stranách '; echo $a; echo ' cm a '; echo $b; echo ' cm'; echo ' je '; echo $obdelnikobsah; echo ' cm^2';
+$strana = 6;
+$uhel = 60;
+$uhelrad = deg2rad($uhel);
 
+$vyska = $strana * sin($uhelrad);
+
+$trojuhelnikobsah = $strana * $vyska /2;
+
+
+echo 'Obsah obdelníku o stranách '; echo $a; echo ' cm a '; echo $b; echo ' cm'; echo ' je '; echo $obdelnikobsah; echo ' cm^2'; ?>
+</br></br>
+    <?php
+echo 'Rovnostranný trojúhelník o straně délky '; echo $strana; echo ' cm a úhlu '; echo $uhel; echo '° je '; echo $trojuhelnikobsah; echo ' cm^2';
 
 /**
  * Created by PhpStorm.
