fe500caede014691139dbcc90d2acaff48e3fb0c
Altusya <goldmoon7@email.cz>
2019-02-26 23:56:41 +0100

    obsah obelniku

---
+++ pythagoras.php
@@ -0,0 +1,14 @@
+<?php
+$a = 3;
+$b = 5;
+$obdelnikobsah = $a * $b;
+
+echo 'Obsah obdelníku o stranách '; echo $a; echo ' cm a '; echo $b; echo ' cm'; echo ' je '; echo $obdelnikobsah; echo ' cm^2';
+
+
+/**
+ * Created by PhpStorm.
+ * User: zhakupovaa
+ * Date: 26.02.2019
+ * Time: 23:33
+ */
